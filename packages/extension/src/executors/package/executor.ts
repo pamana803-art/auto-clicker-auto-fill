95cf49640095bbea3c47321a5645dafd58a0538d
Dharmesh <dharmesh.hemaram@gmail.com>
2023-09-13 17:58:43 +0530

    Removed vulenrabilities

--- packages/extension/src/executors/package/executor.ts
+++ packages/extension/src/executors/package/executor.ts
@@ -1,8 +1,7 @@
 import { PackageExecutorSchema } from './schema';
 import * as webExt from 'web-ext';
-import { ExecutorContext } from '@nx/devkit';
 
-export default async function runExecutor(options: PackageExecutorSchema, context: ExecutorContext) {
+export default async function runExecutor(options: PackageExecutorSchema, context) {
   console.log('Executor ran for Package', options, context);
   console.log('test', Object.keys(context));
   console.log(context.workspace, context.cwd, context.projectName, context.targetName);
