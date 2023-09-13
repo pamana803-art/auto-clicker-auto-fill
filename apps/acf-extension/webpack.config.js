f673bf025f22a3c86b0865de52d773348efc02d7
Dharmesh <dharmesh.hemaram@gmail.com>
2023-09-13 16:44:48 +0530

    re build with

--- apps/acf-extension/webpack.config.js
+++ apps/acf-extension/webpack.config.js
@@ -3,13 +3,13 @@ const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const PACKAGE = require('../../package.json');
 const path = require('path');
-function modify(buffer, version, { KEY, NAME, OAUTH }) {
+function modify(buffer, version, { KEY, NX_NAME, OAUTH }) {
   // copy-webpack-plugin passes a buffer
   const manifest = JSON.parse(buffer.toString());
 
   // make any modifications you like, such as
   manifest.version = version;
-  manifest.name = NAME;
+  manifest.name = NX_NAME;
   if (OAUTH) {
     manifest.oauth2.client_id = OAUTH;
   }
@@ -38,7 +38,7 @@ module.exports = composePlugins(withNx(), (config, ctx) => {
   config.plugins[1] = new CopyPlugin({
     patterns: [
       { from: `**/messages.json`, to: './_locales', context: `${ctx.options.root}/apps/acf-i18n/src/locales` },
-      { from: path.join(__dirname, 'assets', process.env.VARIANT || 'DEV'), to: './assets' },
+      { from: path.join(__dirname, 'assets', process.env.NX_VARIANT || 'DEV'), to: './assets' },
       { from: `./*.html`, to: './html', context: 'src/wizard/popup' },
       { from: `./*.html`, to: './html', context: 'src/sandbox' },
       { from: path.join(ctx.options.root, './node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js'), to: './node_modules' },
