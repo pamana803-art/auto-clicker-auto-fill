e21415ef58b18f6bce316e88dfd2de2793b9f902
Dharmesh <dharmesh.hemaram@gmail.com>
2023-10-12 17:56:54 +0530

    added sentry

---
+++ apps/acf-options-page/src/sentry.ts
@@ -0,0 +1,19 @@
+import * as Sentry from '@sentry/react';
+
+export const sentryInit = () => {
+  Sentry.init({
+    dsn: 'https://aacf1f88c133d2c9b4823c4c0b485ecc@o4506036997455872.ingest.sentry.io/4506037000994816',
+    integrations: [
+      new Sentry.BrowserTracing({
+        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+      }),
+      new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+};
--- apps/acf-options-page/webpack.config.js
+++ apps/acf-options-page/webpack.config.js
@@ -1,3 +1,4 @@
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
 
@@ -5,5 +6,12 @@ const { withReact } = require('@nx/react');
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.plugins.push(
+    sentryWebpackPlugin({
+      org: 'dhruv-techapps',
+      project: 'acf-options-page',
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    })
+  );
   return config;
 });
