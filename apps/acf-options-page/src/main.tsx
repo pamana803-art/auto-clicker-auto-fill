d64841b12baea715a608c026a2ea0b7946479a6b
Dharmesh <dharmesh.hemaram@gmail.com>
2024-04-29 17:23:06 +0530

    removed sentry (#258)

--- apps/acf-options-page/src/main.tsx
+++ apps/acf-options-page/src/main.tsx
@@ -5,13 +5,10 @@ import './i18n';
 import { BROWSER } from './_helpers';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { sentryInit } from './sentry';
 import { TourProvider } from '@reactour/tour';
 import { steps } from './tour';
 
 window.EXTENSION_ID = process.env[`NX_${BROWSER}_EXTENSION_ID`] ?? '';
-
-process.env['NX_VARIANT'] !== 'LOCAL' && sentryInit();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
--- apps/acf-options-page/webpack.config.js
+++ apps/acf-options-page/webpack.config.js
@@ -1,4 +1,3 @@
-const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
 
@@ -6,18 +5,5 @@ const { withReact } = require('@nx/react');
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
-  if (config.mode !== 'development') {
-    config.plugins.push(
-      sentryWebpackPlugin({
-        org: 'dhruv-techapps',
-        project: 'acf-options-page',
-        telemetry: false,
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-        release: {
-          name: process.env.NX_RELEASE_VERSION,
-        },
-      })
-    );
-  }
   return config;
 });
