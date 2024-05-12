28c4ef4ec112041f8189b31d84ffc02097bc1ca2
Dharmesh Patel <dharmesh.hemaram@gmail.com>
2024-05-12 22:47:42 +0530

    feat: Update context menu titles for field configuration, auto clicker, and configuration page

--- apps/acf-extension/src/background/tab.ts
+++ apps/acf-extension/src/background/tab.ts
@@ -3,15 +3,20 @@ import { NotificationHandler } from '@dhruv-techapps/notifications';
 let optionsTab: chrome.tabs.Tab | undefined;
 
 chrome.tabs.onRemoved.addListener((tabId) => {
-  if (optionsTab && optionsTab.id === tabId) {
+  if (optionsTab?.id === tabId) {
     optionsTab = undefined;
   }
 });
 
+const TABS_I18N = {
+  TITLE: chrome.i18n.getMessage('@TABS_TITLE'),
+  ERROR: chrome.i18n.getMessage('@TABS_ERROR'),
+};
+
 const NOTIFICATIONS_ID = 'Tabs Messenger';
 export class TabsMessenger {
   static optionsTab(properties: chrome.tabs.UpdateProperties) {
-    if (optionsTab && optionsTab.id) {
+    if (optionsTab?.id) {
       chrome.tabs.update(optionsTab.id, { ...properties, active: true });
     } else {
       chrome.tabs.create(properties, (tab) => {
@@ -24,7 +29,7 @@ export class TabsMessenger {
     if (sender.tab?.id) {
       chrome.tabs.update(sender.tab.id, updateProperties);
     } else {
-      NotificationHandler.notify(NOTIFICATIONS_ID, 'Tabs', `tab or tabId not found`);
+      NotificationHandler.notify(NOTIFICATIONS_ID, TABS_I18N.TITLE, TABS_I18N.ERROR);
     }
   }
 
@@ -32,7 +37,7 @@ export class TabsMessenger {
     if (sender.tab?.id) {
       chrome.tabs.reload(sender.tab.id);
     } else {
-      NotificationHandler.notify(NOTIFICATIONS_ID, 'Tabs', `tab or tabId not found`);
+      NotificationHandler.notify(NOTIFICATIONS_ID, TABS_I18N.TITLE, TABS_I18N.ERROR);
     }
   }
 }
