4537d1785444a81bf70706e60092bb0dc5d415ac
Dharmesh Patel <dharmesh.hemaram@gmail.com>
2024-05-13 13:16:15 +0530

    updated wizard

--- apps/acf-i18n/src/translate-message.js
+++ apps/acf-i18n/src/translate-message.js
@@ -1,59 +1,55 @@
 const fs = require('fs');
-const translate = require('translate-google');
+const { Translate } = require('@google-cloud/translate').v2;
 const { LANGUAGES } = require('./translate.constant');
+
+// Creates a client
+const translate = new Translate();
+
+async function translateStringValue(value, targetValue, targetLanguage) {
+  if (targetValue) {
+    return targetValue;
+  } else {
+    value = value.replaceAll(/(\$\d+)/g, '{{$1}}');
+
+    let [translatedValue] = await translate.translate(value, { from: 'en', to: targetLanguage });
+    console.log(`Translating "${value}" to "${translatedValue}"`);
+    return translatedValue.replaceAll(/{{(\$\d+)}}/g, '$1');
+  }
+}
 
 // Function to recursively translate the values in an object
 async function translateObject(obj, targetLanguage, targetJson) {
   const translatedObject = {};
 
   for (const key in obj) {
-    // eslint-disable-next-line no-prototype-builtins
-    if (obj.hasOwnProperty(key)) {
-      const value = obj[key];
-      const targetValue = targetJson?.[key];
-
-      if (typeof value === 'string') {
-        if (key === 'message') {
-          if (targetValue !== undefined && targetValue !== null && targetValue !== '') {
-            translatedObject[key] = targetValue;
-          } else {
-            const translatedValue = await translate(value, { to: targetLanguage });
-            console.log(`Translating "${value}" to "${translatedValue}"`);
-            translatedObject[key] = translatedValue;
-          }
-        }
-      } else if (typeof value === 'object') {
-        translatedObject[key] = await translateObject(value, targetLanguage, targetValue);
-      } else {
-        translatedObject[key] = value;
-      }
+    const value = obj[key];
+    const targetValue = targetJson?.[key];
+    if (typeof value === 'string' && key === 'message') {
+      translatedObject[key] = await translateStringValue(value, targetValue, targetLanguage);
+    } else if (typeof value === 'object') {
+      translatedObject[key] = await translateObject(value, targetLanguage, targetValue);
     }
   }
 
   return translatedObject;
 }
 
-// Read the JSON file
-const filePath = 'locales/en/messages.json';
+async function start() {
+  // Read the JSON file
+  const filePath = 'locales/en/messages.json';
+  const englishJson = await fs.promises.readFile(filePath, 'utf8');
 
-fs.readFile(filePath, 'utf8', async (err, data) => {
-  if (err) {
-    console.error('Error reading the file:', err);
-    return;
-  }
+  for (const { lang, folder } of LANGUAGES) {
+    console.error('------------- Processing :: ', folder, lang);
+    const translatedFilePath = `locales/${folder}/messages.json`;
 
-  // Parse the JSON data
-  const englishJson = JSON.parse(data);
-  for (const targetLanguage of LANGUAGES) {
-    const translatedFilePath = `locales/${targetLanguage.folder}/messages.json`;
-
-    const data = await fs.promises.readFile(translatedFilePath, 'utf8');
-    console.error('Processing', targetLanguage.lang);
-    // Parse the JSON data
-    const targetJson = JSON.parse(data);
-
+    const targetJson = await fs.promises.readFile(translatedFilePath, 'utf8').catch(() => {
+      fs.promises.mkdir(`locales/${folder}`, { recursive: true });
+    });
     // Translate the JSON object and log the result
-    const translatedJson = await translateObject(englishJson, targetLanguage.lang, targetJson);
-    await fs.promises.writeFile(`locales/${targetLanguage.folder}/messages.json`, JSON.stringify(translatedJson, null, 2));
+    const translatedJson = await translateObject(JSON.parse(englishJson), lang, JSON.parse(targetJson || '{}'));
+    await fs.promises.writeFile(`locales/${folder}/messages.json`, JSON.stringify(translatedJson, null, 2));
   }
-});
+}
+
+start();
--- apps/acf-i18n/src/translate.constant.js
+++ apps/acf-i18n/src/translate.constant.js
@@ -2,6 +2,18 @@ const LANGUAGES = [
   { lang: 'zh-cn', folder: 'zh_CN' },
   { lang: 'fr', folder: 'fr' },
   { lang: 'ko', folder: 'ko' },
+  { lang: 'sv', folder: 'sv' },
+  { lang: 'pt', folder: 'pt' },
+  { lang: 'ja', folder: 'ja' },
+  { lang: 'nl', folder: 'nl' },
+  { lang: 'de', folder: 'de' },
+  { lang: 'es', folder: 'es' },
+  { lang: 'vi', folder: 'vi' },
+  { lang: 'fi', folder: 'fi' },
+  { lang: 'ar', folder: 'ar' },
+  { lang: 'it', folder: 'it' },
+  { lang: 'ru', folder: 'ru' },
+  { lang: 'id', folder: 'id' },
 ];
 
 module.exports = {
