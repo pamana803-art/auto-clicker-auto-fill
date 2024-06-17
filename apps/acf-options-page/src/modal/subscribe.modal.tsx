5e4d37b34f7890f37a242aebaa506791050c54fd
Dharmesh Pate <dharmesh.hemaram@gmail.com>
2024-06-17 13:55:25 +0530

    removed stripe data

--- apps/acf-options-page/src/modal/subscribe.modal.tsx
+++ apps/acf-options-page/src/modal/subscribe.modal.tsx
@@ -15,7 +15,7 @@ declare global {
   }
 }
 const SubscribeModal = () => {
-  const { visible, products, isSubscribing } = useAppSelector(subscribeSelector);
+  const { visible, isSubscribing } = useAppSelector(subscribeSelector);
   const [yearly, setYearly] = React.useState('year');
   const dispatch = useAppDispatch();
 
@@ -50,73 +50,46 @@ const SubscribeModal = () => {
             </Col>
           </Row>
           <Row>
-            {products?.map((product) => (
-              <React.Fragment key={product.id}>
-                {product.prices
-                  ?.filter((price) => price.recurring.interval === yearly)
-                  .map((price) => (
-                    <Col key={price.id}>
-                      <Card style={{ height: '100%' }}>
-                        <CardBody className='d-flex row-gap-3 flex-column '>
-                          <img src={product.images[0]} width='100px' alt='product' />
-                          <h5>{product.name}</h5>
-                          <div>
-                            <div className='d-flex align-items-center'>
-                              <h1 className='m-0 me-2'>${price.unit_amount ? price.unit_amount / 100 : null}</h1>
-                              <small className='text-grey'>
-                                per
-                                <br />
-                                {price.recurring.interval}
-                              </small>
-                            </div>
-                            <Button variant='primary' disabled={isSubscribing} className='px-5 py-2 mt-3 w-100' onClick={() => dispatch(subscribe(price.id))}>
-                              {isSubscribing && <span className='spinner-border spinner-border-sm me-3' aria-hidden='true'></span>}
-                              Upgrade to {product.name.replace('Auto Clicker AutoFill', '')}
-                            </Button>
-                          </div>
-                          <div>
-                            <p className='p-0 m-0'>This includes:</p>
-                            <ul style={{ listStyle: 'none' }}>
-                              {product.name.includes('Pro') && (
-                                <>
-                                  <li className='text-primary fw-bold'> Plus+</li>
-                                  <li>Google Sheets</li>
-                                  <li>Google Backup</li>
-                                  <li>Discord Messaging</li>
-                                  <li>Captcha resolver</li>
-                                  <li>
-                                    <Discord /> Assistance over Discord
-                                  </li>
-                                  <li>
-                                    <Star /> Early access to new Features
-                                  </li>
-                                  <li>
-                                    <AwardFill /> Pride and Joy
-                                  </li>
-                                </>
-                              )}
-                              {product.name.includes('Plus') && (
-                                <>
-                                  <li>No Ads</li>
-                                  <li>Action Addon</li>
-                                  <li>Action Settings</li>
-                                  <li>Action Condition</li>
-                                  <li>
-                                    <Discord /> Assistance over Discord
-                                  </li>
-                                  <li>
-                                    <Star /> Early access to new Features
-                                  </li>
-                                </>
-                              )}
-                            </ul>
-                          </div>
-                        </CardBody>
-                      </Card>
-                    </Col>
-                  ))}
-              </React.Fragment>
-            ))}
+            <Col>
+              <Card style={{ height: '100%' }}>
+                <CardBody className='d-flex row-gap-3 flex-column '>
+                  <h5>Pro subscription</h5>
+                  <div>
+                    <div className='d-flex align-items-center'>
+                      <h1 className='m-0 me-2'>{yearly === 'year' ? '100' : '10'}$</h1>
+                      <small className='text-grey'>
+                        per
+                        <br />
+                        {yearly}
+                      </small>
+                    </div>
+                    <Button variant='primary' disabled={isSubscribing} className='px-5 py-2 mt-3 w-100' onClick={() => dispatch(subscribe('asd'))}>
+                      {isSubscribing && <span className='spinner-border spinner-border-sm me-3' aria-hidden='true'></span>}
+                      Upgrade to Pro
+                    </Button>
+                  </div>
+                  <div>
+                    <p className='p-0 m-0'>This includes:</p>
+                    <ul style={{ listStyle: 'none' }}>
+                      <li>Google Sheets</li>
+                      <li>Google Backup</li>
+                      <li>Discord Messaging</li>
+                      <li>Captcha resolver</li>
+                      <li>No Ads</li>
+                      <li>
+                        <Discord /> VIP Assistance over Discord
+                      </li>
+                      <li>
+                        <Star /> Early access to new Features
+                      </li>
+                      <li>
+                        <AwardFill /> Pride and Joy
+                      </li>
+                    </ul>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
           </Row>
         </Modal.Body>
       </Form>
