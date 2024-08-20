b1c0c833df1ea394fe77b1d4bffb88a52b12ccf6
Dharmesh Patel <dharmesh.hemaram@gmail.com>
2024-08-20 19:12:32 +0530

    updated command to latest version

--- apps/acf-i18n/src/az-upload-batch.sh
+++ apps/acf-i18n/src/az-upload-batch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-az storage blob upload-batch --account-name autoclickerautofilla579 --destination acf-i18n --source ./locales --pattern "**/web-new.json" --auth-mode login --overwrite
+az storage blob upload-batch --account-name autoclickerautofilla579 --destination acf-i18n --source ./locales --pattern "**/web-new.json" --overwrite --auth-mode login 
