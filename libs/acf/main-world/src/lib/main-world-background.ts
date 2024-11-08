e927216e0e10f26bd1301912f79d4d46886c973d
Dharmesh <dharmesh.hemaram@gmail.com>
2024-11-08 18:44:50 +0530

    Added execution of inline script click within frame (#463)
    * Added execution of click within inline frame
    
    * remove unused .swcrc configuration and update package.json dependencies
    
    * refactor(jest): switch from SWC to ts-jest for test file transformation
    
    * refactor(firebase-storage): switch build executor from SWC to TSC
    
    * chore(firebase-storage): add tslib dependency to package.json

--- libs/acf/main-world/src/lib/main-world-background.ts
+++ libs/acf/main-world/src/lib/main-world-background.ts
@@ -13,7 +13,7 @@ export class MainWorldBackground {
       // Perform the action that requires the permission
       chrome.scripting.executeScript<[string], void>({
         world: 'MAIN',
-        target: { tabId: sender.tab.id },
+        target: { tabId: sender.tab.id, allFrames: true },
         func: (message: string) => {
           window.ACFCommon.default.getElements(message).then((elements: Array<HTMLElement>) => {
             elements.forEach((element) => {
