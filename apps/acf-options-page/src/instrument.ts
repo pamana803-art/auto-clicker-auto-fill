4cd0e8cd4a7eaed7b64737ae10b8dd83d759282e
Dharmesh Patel <dharmesh.hemaram@gmail.com>
2025-01-15 17:32:59 +0530

    Update Sentry DSN environment variables for extension and options page

--- apps/acf-options-page/src/instrument.ts
+++ apps/acf-options-page/src/instrument.ts
@@ -1,8 +1,12 @@
 import * as Sentry from '@sentry/react';
+
 Sentry.init({
-  dsn: process.env.NX_PUBLIC_SENTRY_DSN,
+  dsn: process.env.NX_PUBLIC_OPTIONS_PAGE_SENTRY_DNS,
   environment: process.env.NX_PUBLIC_RELEASE_VERSION === 'v9.9.9' ? 'LOCAL' : process.env.NX_PUBLIC_VARIANT,
   release: `acf-options-page@${process.env.NX_PUBLIC_RELEASE_VERSION?.replace('v', '')}`,
   integrations: [],
-  allowUrls: [/https?:\/\/((dev|beta|stable)\.)?getautoclicker\.com/, /http:\/\/localhost:3000/],
+  beforeSend: (event) => {
+    console.log('Sentry Event:', event);
+    return event;
+  },
 });
