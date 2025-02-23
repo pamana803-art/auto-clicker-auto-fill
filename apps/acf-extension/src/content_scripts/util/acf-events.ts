31c701cc16cfb5a69ab19eac7cd1119495b36ae3
Dharmesh <dharmesh.hemaram@gmail.com>
2025-02-23 10:45:06 +0530

    Improve tag name check for JavaScript links in ACFEvents (#537)

--- apps/acf-extension/src/content_scripts/util/acf-events.ts
+++ apps/acf-extension/src/content_scripts/util/acf-events.ts
@@ -4,7 +4,7 @@ import { MainWorldService } from '@dhruv-techapps/acf-main-world';
 export class ACFEvents {
   static async check(elementFinder: string, elements: Array<HTMLElement>, value?: string) {
     const element = elements[0];
-    if (element.tagName === 'A' && /javascript/gi.test((element as HTMLAnchorElement).href)) {
+    if (/^(A|AREA)$/.test(element.tagName) && /javascript/gi.test((element as HTMLAnchorElement).href)) {
       MainWorldService.click(elementFinder);
       return;
     }
